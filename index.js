79275be980102ff85e5c544114eb9ae1ebed7db6
Fernando <fernandoasfj+github@gmail.com>
2019-06-02 13:51:45 +0200

    Heroku port or default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const glob = require('glob')
 const YAML = require('yaml')
 const fs = require('fs')
